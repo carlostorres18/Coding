18799c41c805b92707b643d264630c102ad6d1ac
carlostorres18 <you@example.com>
2023-07-16 12:24:56 -0500

    added Vectors on the gameGrid.

--- ttt.cpp
+++ ttt.cpp
@@ -6,5 +6,6 @@ using namespace std;
 
 int main(){
    gameGrid();
+   gameGrid2();
 
 }
--- ttt.hpp
+++ ttt.hpp
@@ -5,9 +5,25 @@ using namespace std;
 void gameGrid(){
     for(int j = 0; j < 3; j++){
         for(int i = 0; i < 3; i++){
-            cout<<"[]";
+            cout<<"[choose x or o]";
         }
         cout<<endl;
     }
     cout<<endl;
 }
+
+void playerChoice1(){
+    // Player 1 can only choose X
+}
+void playerChoice2(){
+    // Player 2 can only choose o
+}
+
+void gameGrid2(){
+    // 
+    string answer[9];
+    cout<<"["<<answer[0]<<"]"<<"["<<answer[1]<<"]"<<"["<<answer[2]<<"]"<<endl;
+    cout<<"["<<answer[3]<<"]"<<"["<<answer[4]<<"]"<<"["<<answer[5]<<"]"<<endl;
+    cout<<"["<<answer[6]<<"]"<<"["<<answer[7]<<"]"<<"["<<answer[8]<<"]"<<endl;
+}
+
